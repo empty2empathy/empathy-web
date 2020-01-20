041a46edbcfc599bdf56327c711709e56b8fed61
abelko <esung1129@gmail.com>
2020-01-20 21:25:00 +0900

    modify title to empathy

--- front-empathy/src/App.js
+++ front-empathy/src/App.js
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Empathy☺️</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jin & Abel
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
