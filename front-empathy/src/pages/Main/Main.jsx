42df724a59b791152d82798f45801b002788c33e
Jin <jinsu.kim1011@mapiacompany.com>
2020-02-20 21:49:59 +0900

    add youtube test

--- front-empathy/src/pages/Main/Main.jsx
+++ front-empathy/src/pages/Main/Main.jsx
@@ -1,56 +1,67 @@
-import React from 'react';
-import YouTube from 'react-youtube';
+import React from "react";
+import YouTube from "react-youtube";
 import { Frame } from "framer";
-import './Main.scss';
+import "./Main.scss";
 
 function Main() {
+  const _onReady = event => {
+    // access to player in all event handlers via event.target
+    event.target.playVideo();
+  };
 
-    const _onReady = event => {
-        // access to player in all event handlers via event.target
-        event.target.playVideo();
-    };
+  const opts = {
+    // height: '390',
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      start: 30
+    }
+  };
 
-    const opts = {
-        // height: '390',
-        width: '100%',
-        playerVars: { // https://developers.google.com/youtube/player_parameters
-            start: 30
-        },
-    };
+  return (
+    <div className="Main">
+      <div className="youtube" onClick={_onReady}>
+        <YouTube
+          className="youtube"
+          videoId="QrR_gm6RqCo"
+          opts={opts}
+          onReady={_onReady}
+        />
+        {/* <iframe
+          width="792"
+          height="446"
+          src="https://www.youtube.com/embed/QrR_gm6RqCo"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe> */}
+      </div>
+      <div className="shit">
+        <h1>Service title</h1>
+        <p>
+          description description description description description
+          description
+        </p>
+      </div>
 
-    return (
-        <div className='Main'>
-            <YouTube
-                className='youtube'
-                videoId="bdneye4pzMw"
-                opts={opts}
-                onReady={_onReady}
-            />
-            <div>
-
-            </div>
-            <h1>Service title</h1>
-            <p>description description description description description description</p>
-
-            <div className="performance-info-container">
-                <div>
-                    <div>Fab 17, Mon</div>
-                    <div className="item">
-                        <h4>Event title</h4>
-                    </div>
-                </div>
-            </div>
-
-            <Frame
-                animate={{ rotate: 360 }}
-                transition={{ duration: 1 }}
-                size={150}
-                background={"#f2f2f2"}
-                radius={30}
-            />
+      <div className="performance-info-container">
+        <div>
+          <div>Fab 17, Mon</div>
+          <div className="item">
+            <h4>Event title</h4>
+          </div>
         </div>
-    );
+      </div>
+
+      <Frame
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1 }}
+        size={150}
+        background={"#f2f2f2"}
+        radius={30}
+      />
+    </div>
+  );
 }
 
 export default Main;
-
