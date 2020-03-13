592f5b3d0c0e9365d5fd3cddb5931d3084afe1c5
abelko <esung1129@gmail.com>
2020-03-14 01:12:31 +0900

    Add Youtube player toggling

--- front-empathy/src/constants/domiData.js
+++ front-empathy/src/constants/domiData.js
@@ -112,6 +112,174 @@ export const PERFORMANCE_DATA = [
             'AHADADREAM (UK) @ahadadreamx'
     },
     {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-08 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-08 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-08 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
         title: 'GHETTO-RAY with Ahadadream',
         programType: new Set(['DJING', 'CLUB']),
         location: '이태원 / soap seoul',
@@ -174,6 +342,398 @@ export const PERFORMANCE_DATA = [
         date: {
             start: '2020-02-15 22:00:00',
             end: '2020-02-16 05:00:00'
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-08 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-12 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-12 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-12 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-12 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-12 22:00:00',
+            end: null
+        },
+        artists: [
+            {
+                name: 'AHADADREAM (UK)',
+                instaId: '@ahadadreamx',
+                artistBio: ''
+            },
+            {
+                name: 'ANDOW',
+                instaId: '@andow',
+                artistBio: ''
+            },
+            {
+                name: 'CO.KR',
+                instaId: '@co_kr',
+                artistBio: ''
+            },
+            {
+                name: 'SQUATTERS',
+                instaId: '@squatters_kr',
+                artistBio: ''
+            },
+            {
+                name: 'MONIKA',
+                instaId: '@monikazoo',
+                artistBio: ''
+            },
+            {
+                name: 'VENKO',
+                instaId: '@venkovintage',
+                artistBio: ''
+            },
+            {
+                name: 'JIMMI',
+                instaId: '@jimmijxmmi',
+                artistBio: ''
+            }
+        ],
+        sns: 'https://www.instagram.com/p/B8a-yT8BR8l/',
+        ticketing: false,
+        price: 20000,
+        priceType: 'ADMISSION_FEE',
+        description: '2020년의 첫 GHETTO-RAY 파티가 토요일 Soap Seoul 에 다시 찾아옵니다. 아프리칸 리듬과 트로피컬 샘플 기반의 음악들로 여러분을 사로 잡았던 뜨거운 에너지가 있는 이번 파티에선 스페셜 게스트로 참여하는 Ahadadream (UK) 그리고 무게감있는 로컬 디제이들과 함께 에너지를 느껴보세요.\n' +
+            '⠀\n' +
+            'Ahadadream (UK) 은 런던을 기반으로 Asian Sounds 를 널리 알리고, BBC Asian Network​ 의 레지던시이며, 최근 R&S Records 와 12인치 바이닐 컴필레이션 앨범을 릴리즈 하였던 More Time Records 의 수장인 그와 SOAP seoul 에서 함께합니다.\n' +
+            '⠀\n' +
+            'SPECIAL GUEST :\n' +
+            'AHADADREAM (UK) @ahadadreamx'
+    },
+    {
+        title: '김희나 Quartet',
+        programType: new Set(['JAZZ']),
+        location: 'Bartican(잠실 석촌호수)',
+        date: {
+            start: '2020-02-12 22:00:00',
+            end: null
         },
         artists: [
             {
