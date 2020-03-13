03461322181e30f4846a9a2a901939a175c892c1
abelko <esung1129@gmail.com>
2020-03-13 23:49:56 +0900

    Modify data structure at EventListItem

--- front-empathy/src/components/EventListItem/EventListItem.jsx
+++ front-empathy/src/components/EventListItem/EventListItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './EventListItem.scss';
 
-const EventListItem = ({ title, time, location, date }) => {
+const EventListItem = ({ title, location, date }) => {
     return (
         <div className="EventListItem">
             <div className="event-title-time">
                 <span className="title">{title}</span>
-                <span className="time">{time}</span>
+                <span className="time">{date.start.slice(-8)}</span>
             </div>
             <p className="location-info">{location}</p>
         </div>
