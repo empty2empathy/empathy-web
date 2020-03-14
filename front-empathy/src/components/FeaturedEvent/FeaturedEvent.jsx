2907e13b449faee461cc4ad4ff3bbbf2cfeb2250
Jin <jinsu.kim1011@mapiacompany.com>
2020-03-15 01:00:56 +0900

    Add Jinsu super fucking nice style

--- front-empathy/src/components/FeaturedEvent/FeaturedEvent.jsx
+++ front-empathy/src/components/FeaturedEvent/FeaturedEvent.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
+import React from "react";
 import "./FeaturedEvent.scss";
 import FeaturedEventInfo from "components/FeaturedEventInfo";
 import FeaturedYoutube from "components/FeaturedYoutube";
 
-
 const FeaturedEvent = ({ featuredEvent }) => {
-    if (!featuredEvent) return null;
-    return (
-        <>
-            <FeaturedYoutube youtubeVideoId={featuredEvent.youtubeVideoId}/>
+  if (!featuredEvent) return null;
+  return (
+    <>
+      <FeaturedYoutube youtubeVideoId={featuredEvent.youtubeVideoId} />
 
-            <div className="info-wrapper">
-                <div className="event-title">
-                    <span className="title">{featuredEvent.title}</span>
-                    <span className="now">Now</span>
-                </div>
+      <div className="info-wrapper">
+        <div className="event-title">
+          <span className="title">{featuredEvent.title}</span>
+          <span className="now">Now</span>
+        </div>
 
-                <FeaturedEventInfo featuredEvent={featuredEvent}/>
+        <FeaturedEventInfo featuredEvent={featuredEvent} />
 
-                <div className="insta-share">
-                    <span className="sharing-link">인스타그램 공유하기 &rarr;</span>
-                </div>
-            </div>
-        </>
-    )
+        <div className="insta-share">
+          <span className="sharing-link">인스타그램 공유하기 &rarr;</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default FeaturedEvent
+export default FeaturedEvent;
