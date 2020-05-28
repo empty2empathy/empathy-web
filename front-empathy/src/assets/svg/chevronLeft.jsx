5fd8b4a2f50b5bc44253baeab8e281061207deac
juchanhwang <ghkdwncks96@gmail.com>
2020-05-28 22:03:25 +0900

    refactor(assets/chevronLeft): fix naming to camel case

--- front-empathy/src/assets/svg/chevronLeft.jsx
+++ front-empathy/src/assets/svg/chevronLeft.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default ({ width = 22, height = 22, color = 'white' }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
-    <path d="M 15 18 L 9 12 L 15 6" fill="transparent" stroke-width="2" stroke={color}
-          stroke-miterlimit="10"></path>
+    <path d="M 15 18 L 9 12 L 15 6" fill="transparent" strokeWidth="2" stroke={color}
+          strokeMiterlimit="10"></path>
   </svg>
 )
 
