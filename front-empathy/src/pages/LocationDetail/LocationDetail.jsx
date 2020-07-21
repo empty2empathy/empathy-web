9de44b772b742ee07c2d15e3099d0a6d3a7db9bb
seongjoon <seongjooon@gmail.com>
2020-07-21 23:24:49 +0900

    refactor(src/pages/LocationDetail): optimize rendering to fetch location data

--- front-empathy/src/pages/LocationDetail/LocationDetail.jsx
+++ front-empathy/src/pages/LocationDetail/LocationDetail.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { withFirebase } from "redbricks-firebase";
-import styled from "styled-components";
-import { mapEvents, shareLink } from "../../utils";
-import { withRouter } from "react-router-dom";
-import Map from "../../components/Map";
-import GroupPerDay from "../../components/GroupPerDay";
-import ChevronLeft from "../../assets/svg/chevronLeft";
-import CtaButton from "../../components/CtaButton";
-import CircleItemSlider from "../../components/CircleItemSlider";
-import Loading from "../AddData/_components/Loading";
+import React, { useEffect, useState, useCallback } from 'react';
+import { withFirebase } from 'redbricks-firebase';
+import styled from 'styled-components';
+import { mapEvents, shareLink } from '../../utils';
+import { withRouter } from 'react-router-dom';
+import Map from '../../components/Map';
+import GroupPerDay from '../../components/GroupPerDay';
+import ChevronLeft from '../../assets/svg/chevronLeft';
+import CtaButton from '../../components/CtaButton';
+import CircleItemSlider from '../../components/CircleItemSlider';
+import Loading from '../AddData/_components/Loading';
 
 const fetchData = (url) => {
   return fetch(url)
-    .then(res => res.json())
-    .catch(err => console.error(err));
-}
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
 
 export const getLocationId = (locationId) => {
   return fetchData(`http://localhost:5000/locationDetail/${locationId}`);
@@ -25,7 +25,7 @@ const HeroSection = styled.div`
   position: relative;
   height: 320px;
   overflow: hidden;
-  
+
   img {
     width: 100%;
   }
@@ -45,7 +45,7 @@ const Header = styled.div`
   top: 0;
   color: white;
   text-align: center;
-  
+
   > p {
     flex: 1;
     color: #fcfcfc;
@@ -59,7 +59,7 @@ const Info = styled.div`
   bottom: 16px;
   color: white;
   text-align: center;
-    
+
   p {
     &:nth-child(1) {
       padding: 4px 6px;
@@ -70,7 +70,7 @@ const Info = styled.div`
       font-weight: 700;
       text-shadow: 1px -1px 0px #fcfcfc;
     }
-      
+
     &:nth-child(2) {
       color: #fcfcfc;
       font-size: 36px;
@@ -82,19 +82,20 @@ const Info = styled.div`
 
 const InfoSection = styled.div`
   padding: 30px 20px 0 20px;
-  
+
   > div {
     padding-bottom: 8px;
     font-size: 14px;
     font-weight: 500;
-  
+
     span:first-child {
       padding-right: 8px;
       color: rgba(252, 252, 252, 0.9);
     }
-    
-    span:nth-child(2) {}
-      color: rgba(252, 252, 252, 0.45);
+
+    span:nth-child(2) {
+    }
+    color: rgba(252, 252, 252, 0.45);
   }
 `;
 
@@ -112,7 +113,7 @@ const CtaButtonContainer = styled.div`
 
 const DescriptionSection = styled.div`
   padding: 60px 20px 0 20px;
-  
+
   > p {
     color: rgba(252, 252, 252, 0.6);
   }
@@ -124,33 +125,38 @@ const MapSection = styled.div`
 
 const EventGroup = styled.div`
   display: flex;
-  
+
   .event-list-container {
     padding: 44px 10px 30px;
     background: black;
   }
 `;
 
-const LocationDetail = ({ firebase, match: { params: { locationId } } }) => {
+const LocationDetail = ({
+  firebase,
+  match: {
+    params: { locationId }
+  }
+}) => {
   const [location, setLocation] = useState([]);
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const setLocationDetail = async () => {
+  const setLocationDetail = useCallback(async () => {
     const response = await fetch(`http://localhost:5000/locationDetail/${locationId}`);
     const initialData = await response.json();
     setLocation(initialData);
     setLoading(false);
-  }
+  }, [locationId]);
 
   useEffect(() => {
     const fetchData = async () => {
       const events = await firebase.loadEvents();
-      const parsedEvents$ = events.map(async event => {
+      const parsedEvents$ = events.map(async (event) => {
         const location = await firebase.loadLocationWithPath(event.location.path);
         const artists$ = event.artists.map(({ path }) => firebase.loadArtistWithPath(path));
         const artists = await Promise.all(artists$);
-        return { ...event, location, artists }
+        return { ...event, location, artists };
       });
       const parsedEvents = await Promise.all(parsedEvents$);
       setEvents(mapEvents(parsedEvents));
@@ -160,18 +166,18 @@ const LocationDetail = ({ firebase, match: { params: { locationId } } }) => {
 
   useEffect(() => {
     setLocationDetail();
-  }, [])
+  }, [setLocationDetail]);
 
   return (
     <>
       {loading ? (
-        <Loading/>
+        <Loading />
       ) : (
         <>
           <HeroSection>
-            <img alt="" src={location.image}/>
+            <img alt="" src={location.image} />
             <Header>
-              <ChevronLeft/>
+              <ChevronLeft />
               <p>{location.name}</p>
             </Header>
             <Info>
@@ -181,7 +187,7 @@ const LocationDetail = ({ firebase, match: { params: { locationId } } }) => {
           </HeroSection>
 
           <LocationSlick>
-            <CircleItemSlider slickItems={location.artists}/>
+            <CircleItemSlider slickItems={location.artists} />
           </LocationSlick>
 
           <InfoSection>
@@ -208,34 +214,28 @@ const LocationDetail = ({ firebase, match: { params: { locationId } } }) => {
           </InfoSection>
 
           <CtaButtonContainer>
-            <CtaButton label={`locationDetail`} onClick={() => shareLink(`locationDetail`, `locationDetail`)}/>
+            <CtaButton label={`locationDetail`} onClick={() => shareLink(`locationDetail`, `locationDetail`)} />
           </CtaButtonContainer>
 
           <DescriptionSection>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's
-              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
-              to
-              make
-              a type specimen book. It has survived not only five centuries, but also the leap into electronic
-              typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-              containing
-              Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
-              versions
-              of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It
+              was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
             </p>
           </DescriptionSection>
 
           <MapSection>
-            <Map width='100%' height='270px' latitude={37.7577} longitude={-122.4376} zoom={8}/>
+            <Map width="100%" height="270px" latitude={37.7577} longitude={-122.4376} zoom={8} />
           </MapSection>
 
           <div>
             <EventGroup>
               <div className="event-list-container">
-                {events.map(event => <GroupPerDay key={event.date} {...event} />)}
+                {events.map((event) => (
+                  <GroupPerDay key={event.date} {...event} />
+                ))}
               </div>
             </EventGroup>
           </div>
@@ -243,6 +243,6 @@ const LocationDetail = ({ firebase, match: { params: { locationId } } }) => {
       )}
     </>
   );
-}
+};
 
 export default withRouter(withFirebase(LocationDetail));
